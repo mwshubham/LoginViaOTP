39afbc4f63d7065df62bd40179566cecd19e035d
mwshubham <mwshubham@gmail.com>
2020-03-24 22:17:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/loginviaotp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.loginviaotp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.loginviaotp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+    }
+
+}
